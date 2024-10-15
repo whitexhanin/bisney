637f971bd99fe0c6b0dd080188a686441ff38f25
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-15 16:59:32 +0900

    신규추천영화 api연결

--- Bisney/src/components/NewCinema.tsx
+++ Bisney/src/components/NewCinema.tsx
@@ -1,6 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import {newcinema} from "./newcinema.css";
+import {newcinema , box} from "./newcinema.css";
 import { useEffect, useState } from "react";
 
 // // import 'swiper/swiper-bundle.min.css';
@@ -10,181 +10,101 @@ import { Navigate, redirect, useNavigate, useParams } from "react-router-dom";
 // Swiper 모듈 사용 설정
 // SwiperCore.use([Pagination, Navigation, Autoplay]);
 
-interface Movie {
-    movieCd: string,
-    movieNm: string,
-}
-
 export const NewCinema =  () => {
+  const API_KEY = '2b8dbf43dbbe9ae066cca88158fa0193';
+  const baseURL = 'https://api.themoviedb.org/3';
 
-  //인기 데이터
-  const [popular , setPopular] = useState();
+  
 
-  const fetchPopular = async() => {
-    // try {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,{
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzBmNjNmZTI4OTFkMWE4ZjIyMjgzYTdkNjJiNzMxMiIsIm5iZiI6MTcyODg4MDc0MS4wMzY1NDQsInN1YiI6IjY3MGM4NjBiNGRmNTlhNjA4YzYzNzY2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JI7ppKPLAtd1pBoMM-1grlHUg5S5u9iLg5PiHU2XSME'
+    const fetchMovies = async (endpoint) => {
+        try {
+            const response = await fetch(`${baseURL}/movie/${endpoint}?language=en-US&page=1`, {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzBmNjNmZTI4OTFkMWE4ZjIyMjgzYTdkNjJiNzMxMiIsIm5iZiI6MTcyODg4MDc0MS4wMzY1NDQsInN1YiI6IjY3MGM4NjBiNGRmNTlhNjA4YzYzNzY2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JI7ppKPLAtd1pBoMM-1grlHUg5S5u9iLg5PiHU2XSME'
+                }
+            });
+            console.log('?');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            console.log(result);
+            
+            return result.results.slice(0, 3).map(data => ({
+                id: data.id,
+                title: data.title,
+                poster: data.poster_path
+            }));
+            
+        } catch (error) {          
+            throw new Error(`Failed to fetch data: ${error}`);
         }
-      })
-      .then((response)=>response.json())
-      .catch((error)=> {throw new Error(`${error}`)})
+    };
+
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [topRatedMovies, setTopRatedMovies] = useState([]);
+    const [upcomingMovies, setUpcomingMovies] = useState([]);
+    const [totalData, setTotalData] = useState({});
+    const [loading, setLoading] = useState(true);
+ 
+      const navigate = useNavigate();
+      const onsubScriptPage = (itemname: string) => {
+        console.log(itemname);
+        navigate(`/product/${itemname}`);
+    };
+
+    const removeDuplicates = (array) => {
+      const uniqueIds = new Set();
+      return array.filter(item => {
+          if (!uniqueIds.has(item.id)) {
+              uniqueIds.add(item.id);
+              return true;
+          }
+          return false;
+      });
+    }
 
       
-      const initData = response.results.slice(0,3).map((data)=>{
-        return {
-          title : data.title,          
-          poster : data.poster_path,          
-        }
-      })      
-      setPopular(initData);
-      // if(response.ok){
-      //   const result = await response.json();   
-      //   const top3 =   result.results.slice(0,3)      
-      //   return   top3;
-      // }
-      // else{
-      //   console.error('error',response.statusText);
-      // }
-    // }
-    // catch (error) {
-    //   throw new Error(`Faild to ${error} data`)      
-    // }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const popular = await fetchMovies('popular');
+                const topRated = await fetchMovies('top_rated');
+                const upcoming = await fetchMovies('upcoming');
+                const allMovies = [...popular, ...topRated, ...upcoming];
+                const uniqueMovies = removeDuplicates(allMovies);
 
-  }
-  useEffect(()=>{
-    fetchPopular();
-  },[])
+                // setPopularMovies(popular);
+                // setTopRatedMovies(topRated);
+                // setUpcomingMovies(upcoming);                
+                setTotalData(uniqueMovies);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-  
-console.log(popular);
-  //인기 3개 : fetchPopular
-  //최고평 3개 : fetchToprated
-  //곧 출시될 3개 : fetchUpcoming
-
-//   const fetchPopular = async () => {
-//     const type = 'popular';
-//     await fetchBasic(type);
-//   }
-
-// console.log(fetchPopular())
-//     const { id } = useParams();
-//     const API_KEY = '2b8dbf43dbbe9ae066cca88158fa0193';
-//     const baseURL = 'https://api.themoviedb.org/3/movie/';
-//     const [data, setData] = useState(null);
-//     const [loading, setLoading] = useState(true);
-
-    // const fetchBasic = async (type) => {
-    //     try {
-    //         const response = await fetch(`https://api.themoviedb.org/3/movie/${type}?language=en-US&page=1`,{
-    //           method: 'GET',
-    //           headers: {
-    //             accept: 'application/json',
-    //             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzBmNjNmZTI4OTFkMWE4ZjIyMjgzYTdkNjJiNzMxMiIsIm5iZiI6MTcyODg4MDc0MS4wMzY1NDQsInN1YiI6IjY3MGM4NjBiNGRmNTlhNjA4YzYzNzY2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JI7ppKPLAtd1pBoMM-1grlHUg5S5u9iLg5PiHU2XSME'
-    //           }
-    //         });
-    //         if (response.ok) {
-    //             const result = await response.json();
-    //             console.log(result)
-    //             // setData(result);
-    //             // setLoading(false);
-    //         } else {
-    //             console.error('Error fetching data:', response.statusText);
-    //         }
-    //     } catch (error) {
-    //         console.error('Error fetching data:', error);
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     fetchData();
-    // }, []);
-
-    // if (loading) {
-    //     return <div>로딩 중...</div>;
-    // }
-
-    // if (!data) {
-    //     return <div>데이터가 없습니다</div>;
-    // }
-
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     accept: 'application/json',
-  //     Authorization: 'Bearer 2c0f63fe2891d1a8f22283a7d62b7312'
-  //   }
-  // };
-  
-  // await fetch('https://api.themoviedb.org/3/authentication', options)
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(err => console.error(err));
-
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     accept: 'application/json',
-  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzBmNjNmZTI4OTFkMWE4ZjIyMjgzYTdkNjJiNzMxMiIsIm5iZiI6MTcyODg4MDc0MS4wMzY1NDQsInN1YiI6IjY3MGM4NjBiNGRmNTlhNjA4YzYzNzY2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JI7ppKPLAtd1pBoMM-1grlHUg5S5u9iLg5PiHU2XSME',
-  //   }
-  // };
-
-  // await fetch('https://api.themoviedb.org/3/movie/550', options) // 유효한 엔드포인트로 변경하세요
-  //   .then(response => {
-  //     if (!response.ok) {
-  //       throw new Error('Network response was not ok' + response.statusText);
-  //     }
-  //     return response.json();
-  //   })
-  //   .then(data => console.log(data))
-  //   .catch(err => console.error('Fetch error: ', err));
-
-
-  //   const API_KEY = '2c0f63fe2891d1a8f22283a7d62b7312';
-  //   const baseURL = 'https://www.themoviedb.org/authenticate';
-   
-
-  //   const [data, setData] = useState<Movie[]>([]);
-
-  //   const fetchData = async (date : string) => {
-  //     try {
-  //       const response = await fetch(`${baseURL}/${API_KEY}`, {
-  //         method: 'GET',
-  //       });
-        
-  //       if (response.ok) {
-  //         const result = await response.json();
-  //         setData(result);
-  //         console.log(data);
-  //       } else {
-  //         console.error('Error fetching data:', response.statusText);
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-  
-  //   useEffect(() => {
-  //     fetchData('20241009');
-  //   }, []);
-
-  //   console.log(data);
-  
-  //   if (!data) {
-  //     return <div>로딩 중...</div>;
-  //   }
+        fetchData();
+    }, []);
+    console.log(totalData);
     
-    const navigate = useNavigate();
-    const onsubScriptPage = (itemname: string) => {
-      console.log(itemname);
-      navigate(`/product/${itemname}`);
-  };
+    if (loading) {
+      return <div>로딩 중...</div>;      
+    }  
+    
+
+
+
+
+
 
     return (
         <>
-            {/* <Swiper className={newcinema}
+            <Swiper className={newcinema}
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={50}
             slidesPerView={2}
@@ -194,12 +114,14 @@ console.log(popular);
             onSwiper={(swiper) => console.log(swiper)}
             onSlideChange={() => console.log('slide change')}
             >
-              <SwiperSlide></SwiperSlide> */}
-                {/* {popularMovies?.map((movie )=>(
-                    <SwiperSlide key={movie.id} onClick={()=>onsubScriptPage(movie.id)}>{movie.title}</SwiperSlide>
-                ))}                 */}
-            {/* </Swiper> */}
+              
+                {totalData?.map((movie )=>(
+                    <SwiperSlide key={movie.id} onClick={()=>onsubScriptPage(movie.id)}>
+                      <div style={{width:'100%',height:'100%',background:`url(https://image.tmdb.org/t/p/w500/${movie.poster}) center no-repeat`,backgroundSize:'100%',}}>{movie.title}</div>
+                    </SwiperSlide>
+                ))}                
+            </Swiper>
       </>
     )
 
-}
+  }
--- Bisney/src/components/newcinema.css.ts
+++ Bisney/src/components/newcinema.css.ts
@@ -1,9 +1,24 @@
-import {style , globalStyle} from '@vanilla-extract/css';
+import {style , globalStyle ,  styleVariants} from '@vanilla-extract/css';
 
 export const newcinema = style({
     width: '100%',
     height: '100%',
 })
+
+// export const box = style({
+//   selectors: {
+//     '.newcinema &': {
+//       width:'100%',
+//       height:'100%',
+//       backgroundSize : '100%',      
+//     }
+//   }
+// })
+
+// export const background = styleVariants({
+//   primary: { background: 'blue' },
+//   secondary: { background: 'aqua' }
+// });
 
 globalStyle('.swiper', {
     width: '100%',
