cf4c370f538527871fe1668c3fdf5fbbe383ffe3
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-15 16:59:55 +0900

    검색 바 레이아웃

--- Bisney/src/pages/Search/index.tsx
+++ Bisney/src/pages/Search/index.tsx
@@ -1,6 +1,13 @@
+import * as styles from './styles.css'
+
 const Search  = () => {
     return (
-        <>Search</>
+        <>
+            <div className={styles.searchInput}>
+                <input type="text" placeholder="Search..." className={styles.inputStyle} />
+                <button className={styles.buttonStyle}>Search</button>
+            </div>    
+        </>
     )
 }
 
