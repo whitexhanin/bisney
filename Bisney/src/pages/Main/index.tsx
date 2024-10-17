5968d5fa8bea4768dbfc78f892495c8f710fdd41
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-17 17:02:08 +0900

    / 첫 진입 화면 구현

---
+++ Bisney/src/pages/Main/index.tsx
@@ -0,0 +1,27 @@
+import SignupLayout from "@/layouts/Signup";
+import { Link } from "react-router-dom";
+import  * as styles from './styles.css';
+
+
+const Main = () => {
+    return (
+        <>
+            <SignupLayout>  
+                <div className={styles.main}>
+                    <p className={styles.h2}>
+                        이 모든 이야기가 여기에
+                        지금 스트리밍 중
+                    </p>   
+                    <p>지금 가입 하세요.</p>
+                    <Link className={styles.Link} to='/signup/Create-email'>SignUp</Link>   
+                    <br/>
+                    <p>이미 디즈니에 가입하셨나요?</p>
+                    <Link className={styles.Link} to='/login'>Login</Link>
+                </div>
+            </SignupLayout>
+        </>
+    )
+}
+
+export default Main;
+
---
+++ Bisney/src/pages/Main/styles.css.tsx
@@ -0,0 +1,40 @@
+import { globalStyle, style } from "@vanilla-extract/css";
+
+
+
+export const h2 = style({
+    fontSize:'30px',
+    fontWeight:'bold',
+    color:'#fff',
+})
+
+export const Link = style({
+    display:'block',
+    textAlign:'center',
+    width : '275px',
+    height:'60px',
+    lineHeight : '60px',
+    background:'#02d6e8',
+    borderRadius:'4px',
+    fontSize:'18px',
+    color:'#02172a',
+    selectors : {
+        ['&:hover'] : {
+            filter:'brightness(.85)',
+            textDecoration: 'none',
+            color:'#02172a',
+        }
+    }
+})
+
+export const main = style({
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+})
+
+globalStyle(`${main} p` , {
+    color:'#fff',
+})
+
