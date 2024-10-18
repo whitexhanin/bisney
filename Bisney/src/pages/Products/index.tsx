71ecb2d45a34caa996d9a0bc1d5eb4df52ba70b4
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-18 17:03:52 +0900

    네이밍 변경

--- Bisney/src/pages/Products/index.tsx
+++ Bisney/src/pages/Products/index.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 
-export default function Product () {
+export default function Products () {
     return (
         <>
         Products
         <div>
+            <div>리스트 가져올꺼야?</div>
             <Outlet/>
         </div>
         </>
