3466b77ff2f73dde904cd969ab7eaa9418564474
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-24 14:36:59 +0900

    로고 주석 삭제

--- Bisney/src/pages/Home/index.tsx
+++ Bisney/src/pages/Home/index.tsx
@@ -1,16 +1,25 @@
 import { NewCinema } from '@/components/NewCinema';
-import  {home , logo} from './styles.css';
+import  {home , logo , img} from './styles.css';
+import Recommend1 from '@/components/Recommend1';
+import RecommendList from '@/components/RecommendList';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const Home  = () => {
     return (
         <>
             <div className={home}>
                 <h2 className={logo}>
-                    {/* <img className={img} src={`/logo/original.png`} alt="Disney+" /> */}
+                    <img className={img} src={`/logo/original.png`} alt="Disney+" />
                 </h2>
                 {/* 최신/인기 슬라이드 */}
-                <NewCinema/>
+                <QueryClientProvider client={queryClient}>
+                    <NewCinema/>
+                </QueryClientProvider>
                 {/* diseny,pixar ,marvel,starwars,nationalgeographic,star 채널 버튼 */}
+                <RecommendList />
+                {/* <Recommend2 /> */}
                 {/* 오늘밤 뭐볼까 추천 아이템 나열 슬라이드  디자인 1 - 작은사이즈*/}
                 {/* 디즈니+ 최신작 아이템 나열 슬라이드 디자인 1 - 작은사이즈*/}
                 {/* 시청중인콘텐츠 */}
