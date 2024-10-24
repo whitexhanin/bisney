9036f7361983b1607fecbabff273931c762f0f62
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-24 14:35:23 +0900

    검색 내 메뉴 컴포넌트 생성

--- Bisney/src/components/SearchMenu.tsx
+++ Bisney/src/components/SearchMenu.tsx
@@ -9,7 +9,7 @@ const SearchMenu = () => {
                 {/* 동일 page에 파라미터 주고 받아서 api 끌고와 작성하기 */}
                 <Link to={'/productlist/original'}>오리지널</Link>
                 <Link to={'/productlist/movie'}>영화</Link>
-                <Link to={'/productlist/series'}>시리즈</Link>
+                <Link to={'/productlist/tv'}>시리즈</Link>
             </div>
         </>
     )
