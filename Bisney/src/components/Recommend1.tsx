aa2c85d6dea4509bb376316a19039cfe84b69878
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-24 14:35:02 +0900

    추천 컴포넌트 생성

---
+++ Bisney/src/components/Recommend1.tsx
@@ -0,0 +1,13 @@
+const Recommend1 = () => {
+    return (
+        <>
+            <h2>Title Name</h2>
+            <div>
+                데이터 스와이프 리스트 내용 
+            </div>
+            
+        </>
+    )
+}
+
+export default Recommend1;
---
+++ Bisney/src/components/RecommendList.tsx
@@ -0,0 +1,11 @@
+import Recommend1 from "./Recommend1";
+
+const RecommendList = () => {
+    return (
+        <>
+            <Recommend1/>
+        </>
+    )
+}
+
+export default RecommendList;
