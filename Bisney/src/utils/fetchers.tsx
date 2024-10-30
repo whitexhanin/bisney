1987a0b82397c61c39c4e1d0e69935f1318c6062
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-30 16:52:05 +0900

    options 추가

--- Bisney/src/utils/fetchers.tsx
+++ Bisney/src/utils/fetchers.tsx
@@ -1,3 +1,42 @@
-export const fetchers = (url) => {
-    fetch(url).then(res => res.json())
+export const fetchers = async (url) => {
+    // const res = await fetch(url);
+
+    
+
+    // if(!res.ok) {
+    //     const error = new Error('An error')
+
+    //     error.info = await res.json();
+    //     error.status = res.status
+    //     throw error
+    // }
+
+    // return res.json()
+
+    // fetch(url).then(res => res.json())
+
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzBmNjNmZTI4OTFkMWE4ZjIyMjgzYTdkNjJiNzMxMiIsIm5iZiI6MTczMDE2NjEwMi4zNTg3NDksInN1YiI6IjY3MGM4NjBiNGRmNTlhNjA4YzYzNzY2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Rd6L2-PKHAmYHVPgYHvdacAs3JO9LvcPzokfb22zKU'
+        }
+      };
+    // const response = await fetch(u)
+    const response = await fetch(url , options);
+    if(!response.ok) {
+        throw new Error('Network');
+    }
+    const data = await response.json();
+
+    return  data;
+
+
+    //   await fetch(url, options)
+    //     .then(res => await res.json())
+    //     .then(res => console.log(res))
+    //     .catch(err => console.error(err));
 }
+
+
