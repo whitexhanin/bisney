fc366998dc38d3f87c2308cd1cb3469064298c3f
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-30 16:55:17 +0900

    form 태그 구조로 변경

--- Bisney/src/components/SearchInput.tsx
+++ Bisney/src/components/SearchInput.tsx
@@ -1,13 +1,25 @@
+import { fetchers } from '@/utils/fetchers';
 import * as styles from './searchinput.css'
+import useSWR from 'swr';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchInput = () => {
+    const navigate = useNavigate();
+    const [inputvalue , setInputvalue] = useState();    
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        navigate(`/search?q=${e.currentTarget.search.value}`);
+    }
 
     return(
-        <>
-            <div className={styles.searchInput}>
-                <input type="text" placeholder="Search..." className={styles.inputStyle} />
-                <button className={styles.buttonStyle}>Search</button>
-            </div>          
+        <>  <form onSubmit={onSubmit}>
+                <div className={styles.searchInput}>
+                    <input name = "search" type="search" value ={inputvalue} placeholder="Search..." className={styles.inputStyle} />
+                    <button className={styles.buttonStyle}>Search</button>
+                </div>          
+            </form>
         </>
     )
 
