7735dc402a99aab5cd179ea087d1a0e1afd7da9e
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-10-30 16:53:51 +0900

    검색 결과 리스트 api 연결

--- Bisney/src/components/CollectionList.tsx
+++ Bisney/src/components/CollectionList.tsx
@@ -1,6 +1,33 @@
+import * as styles from './collectionlist.css';
+import { fetchers } from "@/utils/fetchers";
+import { useLocation, useParams } from "react-router-dom";
+import useSWR from "swr";
+
 const CollectionList = () => {
-    return (
-        <></>
+
+    const location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const value = query.get('q');
+    const {data , error} =  useSWR(`https://api.themoviedb.org/3/search/multi?query=${value}&include_adult=false&language=en-US&page=1`,fetchers)    
+
+    if (error) console.error(error);
+    if (!data) console.log('Loading...');
+    if (data) console.log(data);
+
+    return (       
+        <>
+            <h2 className={styles.h2}>{(value)?.toUpperCase()}</h2>
+            {/* 리스트 아이템 클릭 시 products/:id 로 이동*/}
+            <div className={styles.itemcontainer}>
+                <div className={styles.itemlist}>
+                    {
+                        data?.results?.map((d)=>(
+                            <div key={d.id} className={styles.item} style={{background:`url(https://image.tmdb.org/t/p/w500/${d.poster_path}) center no-repeat`,backgroundSize:'cover',}}></div>                    
+                        ))
+                    }
+                </div>
+            </div>
+        </>
     )
 }
 export default CollectionList;
